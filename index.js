b602c24f408cad90896ed6f8b49204c7c070bd62
Amir Hosein Nazari <nazari.amirhosein79@gmail.com>
2022-07-24 22:38:03 +0400

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-// const app = express();
 const shortUrl = require('./routes/api/shorturl');
 
 app.use(express.urlencoded({ extended: false }));
@@ -19,18 +18,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
 app.get('/api/hello', function (req, res) {
-  // res.json({ greeting: 'hello API' });
-
   res.redirect('https://www.google.com');
 });
 
-mongoose.connect(process.env.PORT || 3000, { useNewUrlParser: true });
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('connected to db'));
-const port = process.env.PORT || 3000;
+
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
